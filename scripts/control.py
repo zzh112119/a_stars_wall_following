408b733627570629e1d2a3c0cd5b4f06b9f4d595
zhiheng <zzh112119@gmail.com>
2018-09-30 13:25:45 -0400

    finished structure of the code and haven't tuned pid yet

---
+++ scripts/control.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+
+import rospy
+from race.msg import drive_param
+from std_msgs.msg import Float64
+import numpy as np
+import math
+
+class pid_controller:
+
+	def __init__(self, P=0.0, I=0.0, D=0.0):
+
+		self.Kp = P
+		self.Ki = I
+		self.Kd = D
+
+		self.sample_time = 0.00
+		self.current_time = rospy.get_time()
+		self.last_time = self.current_time
+		self.SetPoint = 0.0
+
+		self.clear()    
+
+	def clear(self):
+		"""Clears PID computations and coefficients"""
+		self.SetPoint = 0.0
+
+		self.PTerm = 0.0
+		self.ITerm = 0.0
+		self.DTerm = 0.0
+		self.last_error = 0.0
+
+		# Windup Guard
+		self.int_error = 0.0
+		self.windup_guard = 20.0
+
+		self.output = 0.0
+
+	def update(self, feedback_value):
+		"""Calculates PID value for given reference feedback
+		.. math::
+			u(t) = K_p e(t) + K_i \int_{0}^{t} e(t)dt + K_d {de}/{dt}
+		.. figure:: images/pid_1.png
+		   :align:   center
+		   Test PID with Kp=1.2, Ki=1, Kd=0.001 (test_pid.py)
+		"""
+		error = feedback_value - self.SetPoint 
+		# print(self.SetPoint)
+		# //print("ERROR:",error)
+		self.current_time = rospy.get_time()
+		delta_time = self.current_time - self.last_time
+		#delta_time = 1.0 / self.frequency
+		delta_error = error - self.last_error
+
+		if (delta_time >= self.sample_time):
+			self.PTerm = self.Kp * error
+			self.ITerm -= error * delta_time
+
+			# if (self.ITerm < -self.windup_guard):
+			#     self.ITerm = -self.windup_guard
+			# elif (self.ITerm > self.windup_guard):
+			#     self.ITerm = self.windup_guard
+
+			# self.DTerm = 0.0
+			if delta_time > 0:
+				self.DTerm = delta_error / delta_time
+
+			# Remember last time and last error for next calculation
+			self.last_time = self.current_time
+			self.last_error = error
+
+			self.output = - self.PTerm + (self.Ki * self.ITerm) - (self.Kd * self.DTerm)
+
+		return self.output
+		# rospy.loginfo(self.output)
+
+	def setKp(self, proportional_gain):
+		"""Determines how aggressively the PID reacts to the current error with setting Proportional Gain"""
+		self.Kp = proportional_gain
+
+	def setKi(self, integral_gain):
+		"""Determines how aggressively the PID reacts to the current error with setting Integral Gain"""
+		self.Ki = integral_gain
+
+	def setKd(self, derivative_gain):
+		"""Determines how aggressively the PID reacts to the current error with setting Derivative Gain"""
+		self.Kd = derivative_gain
+
+	def setWindup(self, windup):
+		"""Integral windup, also known as integrator windup or reset windup,
+		refers to the situation in a PID feedback controller where
+		a large change in setpoint occurs (say a positive change)
+		and the integral terms accumulates a significant error
+		during the rise (windup), thus overshooting and continuing
+		to increase as this accumulated error is unwound
+		(offset by errors in the other direction).
+		The specific problem is the excess overshooting.
+		"""
+		self.windup_guard = windup
+
+	def setSampleTime(self, sample_time):
+		"""PID that should be updated at a regular interval.
+		Based on a pre-determined sampe time, the PID decides if it should compute or return immediately.
+		"""
+		self.sample_time = sample_time
+
+class control_loop:
+
+	def __init__(self):
+		# TODO: modify these constants to make the car follow walls smoothly.
+		self.KP = 5
+		self.KD = 0.3
+		self.last_angle = 0.0
+		self.last_vel = 0.5
+		self.pid_last = 0.0
+		self.msg = drive_param()
+
+		self.pub = rospy.Publisher('drive_parameters', drive_param, queue_size=1)
+		self.pid = pid_controller()	
+
+		self.max_rad_angle = 30*math.pi/180
+		rospy.Subscriber("pid_error", Float64, self.control_callback)
+
+
+	def calculate_angle(self,data,vel):
+		self.pid.setKp(self.KP)
+		self.pid.setKd(self.KD)
+		self.pid.SetPoint = 0.0
+		self.pid.setSampleTime = 1.0 / 40.0
+		output_angle = self.pid.update(data.data)	
+		output_angle = np.clip(output_angle,-self.max_rad_angle,self.max_rad_angle)
+		# pid_now, delta_time= self.pid.update(data.data)	
+		# print(pid_now,self.pid_last,delta_time,vel)
+		# output_angle = math.acos((pid_now - self.pid_last)/delta_time/vel)
+
+		# self.pid_last = pid_now
+		return output_angle
+
+	def calculate_vel(self,angle):
+		vel = 0.0
+		angle_d = abs(angle * 180 / math.pi)
+		if (angle_d>=0) and (angle_d<10):
+			vel = 1.5
+		elif (angle_d>=10) and (angle_d<20):
+			vel = 1.0
+		else:
+			vel = 0.5
+		return vel
+
+
+	# Callback for receiving PID error data on the /pid_error topic
+	# data: the PID error from pid_error_node, published as a Float64
+	def control_callback(self,data):
+	  # TODO: Based on the error (data.data), determine the car's required velocity
+	  # amd steering angle.
+		# self.pid.setKp(self.KP)
+		# self.pid.setKd(self.KD)
+
+		self.msg.angle = self.calculate_angle(data,self.last_vel)   # TODO: implement PID for steering angle
+		self.msg.velocity = self.calculate_vel(self.last_angle)  # TODO: implement PID for velocity
+	 	self.last_vel = self.msg.velocity
+		self.last_angle = self.msg.angle
+
+	def send_command(self):
+
+		self.pub.publish(self.msg)
+		print("SENT")
+
+# Boilerplate code to start this ROS node.
+# DO NOT MODIFY!
+if __name__ == '__main__':
+	rospy.init_node('pid_controller_node', anonymous=True)
+	C = control_loop()	
+	r = rospy.Rate(40)
+
+	while not rospy.is_shutdown():
+		C.send_command()
+		r.sleep()
+
+
---
+++ scripts/pid_error.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+import rospy
+from math import cos, sin, atan, pi
+import numpy as np
+import yaml
+import sys
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Float64
+import pdb
+
+pub = rospy.Publisher('pid_error', Float64, queue_size=10)
+
+# You can define constants in Python as uppercase global names like these.
+MIN_DISTANCE = 0.1
+MAX_DISTANCE = 30.0
+MIN_ANGLE = -45.0
+MAX_ANGLE = 225.0
+a = 0.0
+b = 0.0
+# data: single message from topic /scan
+# angle: between -45 to 225 degrees, where 0 degrees is directly to the right
+# Outputs length in meters to object with angle in lidar scan field of view
+def getRange(data, angle, old_range):
+
+  ranges = np.asarray(data.ranges)
+  # Create angle features
+  # angles_raw = np.arange(data.angle_min, data.angle_max, data.angle_increment)
+  # angles = [i*180/pi for i in angles_raw]
+  # increment = data.angle_increment * 180 / math.pi
+  # angle_index = np.where(angles<=(angle+increment/2) or angles>(angle-increment/2))
+  angle_index = (angle + 45) * 4
+  if ranges[angle_index] == np.nan or ranges[angle_index] == np.inf:
+    return old_range
+  else:
+    output_range = ranges[angle_index]
+    return output_range
+
+# data: single message from topic /scan
+# desired_distance: desired distance to the left wall [meters]
+# Outputs the PID error required to make the car follow the left wall.
+def followLeft(data, desired_distance):
+  global a,b
+  L = 0.025
+  desired_distance = desired_distance
+  a = getRange(data, 135, a)
+  b = getRange(data, 180, b)
+  theta = 45 * pi / 180
+  alpha = atan((a*cos(theta)-b)/(a*sin(theta)))
+  current_dist  = b * cos(alpha)
+  next_dist = current_dist + L * sin(alpha)
+  error_t = desired_distance - current_dist
+
+  # pass the error_t term into some function and output the next_angle and velocity
+  return error_t
+
+# data: single message from topic /scan
+# desired_distance: desired distance to the right wall [meters]
+# Outputs the PID error required to make the car follow the right wall.
+def followRight(data, desired_distance):
+  global a,b
+  L = 0.025
+  desired_distance = desired_distance
+  a = getRange(data, 45, a)
+  b = getRange(data, 0, b)
+  theta = 45 * pi / 180
+  alpha = atan((a*cos(theta)-b)/(a*sin(theta)))
+  current_dist  = b * cos(alpha)
+  next_dist = current_dist + L * sin(alpha)
+  error_t = desired_distance - current_dist
+
+  # TODO: implement
+  return -error_t
+
+# data: single message from topic /scan
+# Outputs the PID error required to make the car drive in the middle
+# of the hallway.
+def followCenter(data):
+  left_error = followLeft(data,1)
+  right_error = followRight(data,1)
+  center_error = (left_error + right_error)/2
+
+  return center_error
+
+# Callback for receiving LIDAR data on the /scan topic.
+# data: the LIDAR data, published as a list of distances to the wall.
+def scan_callback(data):
+  desired_distance = 0.8
+  # error = followLeft(data,desired_distance) 
+  # error = followRight(data,desired_distance) 
+  error = followCenter(data) 
+
+  msg = Float64()
+  msg.data = error
+  pub.publish(msg)
+
+# Boilerplate code to start this ROS node.
+# DO NOT MODIFY!
+if __name__ == '__main__':
+	rospy.init_node('pid_error_node', anonymous = True)
+	rospy.Subscriber("scan", LaserScan, scan_callback)
+	rospy.spin()
